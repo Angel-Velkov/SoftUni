e602a8f658453ab69a4fd5ec0e1d4a55f9329bc5
Angel Velkov <angel.velkov13.06@gmail.com>
2021-01-31 16:25:24 +0200

    Added the rest of the exercises and the conditions

--- Java-Advanced/FunctionalProgramming/src/Exercise/ReverseAndExclude.java
+++ Java-Advanced/FunctionalProgramming/src/Exercise/ReverseAndExclude.java
@@ -1,8 +1,6 @@
 package Exercise;
 
 import java.util.*;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -10,26 +8,7 @@ public class ReverseAndExclude {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        BiFunction<Integer, Integer, Boolean> isNotDivisibleBy = (n, d) -> (n % d) != 0;
-        Function<List<Integer>, String> arrayAsString =
-                nums -> nums.stream().map(String::valueOf).collect(Collectors.joining(" "));
-
-        List<Integer> numbers = Arrays.stream(scanner.nextLine()
-                .split("\\s+"))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-
-        Collections.reverse(numbers);
-        int divisor = Integer.parseInt(scanner.nextLine());
-
-        numbers = numbers.stream().filter(n -> isNotDivisibleBy.apply(n, divisor)).collect(Collectors.toList());
-
-        System.out.println(arrayAsString.apply(numbers));
-
-
-         /*
-        List<Integer> numbers = Arrays.stream(scanner.nextLine()
-                .split("\\s+"))
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
 
@@ -37,10 +16,11 @@ public class ReverseAndExclude {
 
         int divisor = Integer.parseInt(scanner.nextLine());
 
-        Predicate<Integer> isNotDivisibleBy = n -> n % divisor != 0;
+        Predicate<Integer> isDivisibleBy = n -> n % divisor == 0;
 
-        numbers.stream().filter(isNotDivisibleBy).forEach(n -> System.out.print(n + " "));
+        numbers.removeIf(isDivisibleBy);
 
-          */
+        numbers.forEach(n -> System.out.print(n + " "));
+
     }
 }
