156cb0f0b036840d88f8b39b499d52c95c6deb21
Angel Velkov <angel.velkov13.06@gmail.com>
2021-02-04 21:56:19 +0200

    Added solution

---
+++ Java-Advanced/WorkshopCustomDataStructures/src/Lab/SmartArray/Main.java
@@ -0,0 +1,12 @@
+package Lab.SmartArray;
+
+public class Main {
+    public static void main(String[] args) {
+        SmartArray smartArray = new SmartArray();
+
+        smartArray.add(1);
+        for (int i = 1; i < 100; i++) {
+            smartArray.add(0, i + 1);
+        }
+    }
+}
