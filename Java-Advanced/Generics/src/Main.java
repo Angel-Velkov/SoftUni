4e22c5607b6e9c66d745fd478dd5a2012940fb41
Angel Velkov <angel.velkov13.06@gmail.com>
2021-02-09 22:39:11 +0200

    Added 4 solutions for 8 exercises (each solution works for two exercise) Added MY CustomList

--- Java-Advanced/Generics/src/Main.java
+++ Java-Advanced/Generics/src/Main.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
+        /*
         List elements = new ArrayList();
 
         elements.add("process");
@@ -13,6 +13,18 @@ public class Main {
             System.out.println(element instanceof String);
             System.out.println(element instanceof Integer);
             System.out.println("-----------");
+        }
+
+         */
+
+        CustomList<Integer> list = new CustomList<>(Integer.class);
+
+        for (int i = 0; i < 10; i++) {
+            list.add(i + 1);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(list.remove(i));
         }
     }
 }
