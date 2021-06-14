dd30d4ebd2227c7c14f696af6d8c9f3e6f667f3d
Angel-Velkov <76847316+Angel-Velkov@users.noreply.github.com>
2021-06-14 17:26:07 +0300

    Fixed `deleted_employees` table and added data to a new table `employees` that has no constraints.

--- MySQL/DatabaseProgrammabilityAndTransactions/Solutions/Lab/04Triggered.sql
+++ MySQL/DatabaseProgrammabilityAndTransactions/Solutions/Lab/04Triggered.sql
@@ -1,16 +1,40 @@
+DROP DATABASE IF EXISTS `soft_uni`;
+CREATE DATABASE `soft_uni`;
 USE `soft_uni`;
 
-CREATE TABLE `deleted_employeest` (
-  `employee_id` int NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `middle_name` varchar(50) DEFAULT NULL,
-  `job_title` varchar(50) NOT NULL,
-  `department_id` int NOT NULL,
-  `manager_id` int DEFAULT NULL,
-  `hire_date` timestamp(6) NOT NULL,
-  `salary` decimal(19,4) NOT NULL,
-  `address_id` int DEFAULT NULL
+CREATE TABLE `employees`(
+	`employee_id` INT PRIMARY KEY AUTO_INCREMENT,
+	`first_name` VARCHAR(50),
+	`last_name` VARCHAR(50),
+	`middle_name` VARCHAR(50),
+	`job_title` VARCHAR(50),
+	`department_id` INT,
+	`salary` DECIMAL(19,4)
+);
+
+INSERT INTO `employees`
+VALUES
+	(1, 'Guy', 'Gilbert', 'R', 'Production Technician', 7, 12500.0000),
+	(2, 'Kevin', 'Brown', 'F', 'Marketing Assistant', 4, 13500.0000),
+	(3, 'Roberto', 'Tamburello', NULL, 'Engineering Manager', 1, 43300.0000),
+	(4, 'Rob', 'Walters', NULL, 'Senior Tool Designer', 2, 29800.0000),
+	(5, 'Thierry', 'D\'Hers', 'B', 'Tool Designer', 2, 25000.0000),
+	(6, 'David', 'Bradley', 'M', 'Marketing Manager', 5, 37500.0000),
+	(7, 'JoLynn', 'Dobney', 'M', 'Production Supervisor', 7, 25000.0000),
+	(8, 'Ruth', 'Ellerbrock', 'Ann', 'Production Technician', 7, 13500.0000),
+	(9, 'Gail', 'Erickson', 'A', 'Design Engineer', 1, 32700.0000),
+	(10, 'Barry', 'Johnson', 'K', 'Production Technician', 7 , 13500.0000)
+;
+
+
+CREATE TABLE `deleted_employees` (
+	`employee_id` INT PRIMARY KEY AUTO_INCREMENT,
+	`first_name` VARCHAR(50),
+	`last_name` VARCHAR(50),
+	`middle_name` VARCHAR(50),
+	`job_title` VARCHAR(50),
+	`department_id` INT,
+	`salary` DECIMAL(19,4)
 );
 
 DELIMITER $$
@@ -19,19 +43,16 @@ AFTER DELETE
 ON `employees`
 FOR EACH ROW
 BEGIN
-INSERT INTO `soft_uni`.`deleted_employeest`
-(`employee_id`,
-`first_name`,
-`last_name`,
-`middle_name`,
-`job_title`,
-`department_id`,
-`manager_id`,
-`hire_date`,
-`salary`,
-`address_id`)
+INSERT INTO `deleted_employees`
+	(`first_name`, `last_name`, `middle_name`, `job_title`,
+		`department_id`, `salary`)
 VALUES
-(OLD.`employee_id`, OLD.`first_name`, OLD.`last_name`, OLD.`middle_name`, OLD.`job_title`,
-	OLD.`department_id`, OLD.`manager_id`, OLD.`hire_date`, OLD.`salary`, OLD.`address_id`);
+(OLD.`first_name`, OLD.`last_name`, OLD.`middle_name`, OLD.`job_title`,
+	OLD.`department_id`, OLD.`salary`);
 END$$
 DELIMITER ;
+
+SET SQL_SAFE_UPDATES = 0;
+
+DELETE FROM `employees`
+WHERE `employee_id` = 1;
