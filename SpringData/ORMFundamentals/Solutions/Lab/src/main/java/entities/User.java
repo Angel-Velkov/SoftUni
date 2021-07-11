b7ba5eec8e3315f0e0c9f0b41ab478b0c42f9c30
Angel-Velkov <76847316+Angel-Velkov@users.noreply.github.com>
2021-07-11 23:40:06 +0300

    Added half of the custom ORM witch I have as a condition.

---
+++ SpringData/ORMFundamentals/Solutions/Lab/src/main/java/entities/User.java
@@ -0,0 +1,93 @@
+package entities;
+
+import orm.annotations.Column;
+import orm.annotations.Entity;
+import orm.annotations.Id;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Entity(name = "USERS")
+public class User {
+    @Id
+    private int id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "age")
+    private int age;
+    @Column(name = "registration_date")
+    private Date registrationDate;
+
+    public User(String username, String password, int age, Date registrationDate) {
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.registrationDate = registrationDate;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getRegistrationDate() {
+        return this.registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                ", registrationDate='" + registrationDate + '\'' +
+                '}';
+    }
+}
