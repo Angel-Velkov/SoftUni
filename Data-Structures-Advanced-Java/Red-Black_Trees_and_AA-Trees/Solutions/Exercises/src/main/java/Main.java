28b998f2d8b47f7f69b61ca7eeac5368b7d76c87
Angel-Velkov <76847316+Angel-Velkov@users.noreply.github.com>
2021-09-12 23:34:47 +0300

    Added implementation of AA-Tree

--- Data-Structures-Advanced-Java/Red-Black_Trees_and_AA-Trees/Solutions/Exercises/src/main/java/Main.java
+++ Data-Structures-Advanced-Java/Red-Black_Trees_and_AA-Trees/Solutions/Exercises/src/main/java/Main.java
@@ -1,13 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        String[] input = {"S", "E", "A", "R", "C", "H", "E", "X", "A", "M", "P", "L", "E"};
-
-        RedBlackTree<String, Integer> redBlackTree = new RedBlackTree<>();
-
-        for (int i = 0; i < input.length; i++) {
-            redBlackTree.put(input[i], i);
-            redBlackTree.printPreOrder();
-            System.out.println("<=================================>");
-        }
     }
 }
