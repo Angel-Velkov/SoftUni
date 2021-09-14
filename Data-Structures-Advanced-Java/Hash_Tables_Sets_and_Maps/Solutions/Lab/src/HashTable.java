0473d8d0b943ee4dd9552d80156c02edaf8de6d4
Angel-Velkov <76847316+Angel-Velkov@users.noreply.github.com>
2021-09-14 19:12:37 +0300

    Made the class iterable and implemented the method to double the capacity.

--- Data-Structures-Advanced-Java/Hash_Tables_Sets_and_Maps/Solutions/Lab/src/HashTable.java
+++ Data-Structures-Advanced-Java/Hash_Tables_Sets_and_Maps/Solutions/Lab/src/HashTable.java
@@ -1,26 +1,24 @@
 import java.lang.reflect.Array;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.function.Consumer;
 
-public class HashTable<K, V> {
-    private static class KeyValue<K, V> {
-        K key;
-        V value;
-
-        KeyValue(K key, V value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
-
+public class HashTable<K, V> implements Iterable<KeyValue<K, V>> {
     private static final int INITIAL_CAPACITY = 16;
     private static final double MAX_LOAD_FACTOR = 0.7;
 
-    private final LinkedList<KeyValue<K, V>>[] table;
+    private LinkedList<KeyValue<K, V>>[] table;
     private int size;
+    private int capacity;
 
     public HashTable() {
-        this.table = createTable(INITIAL_CAPACITY);
+        this(INITIAL_CAPACITY);
+    }
+
+    private HashTable(int capacity) {
+        this.table = createTable(capacity);
         this.size = 0;
+        this.capacity = capacity;
     }
 
     public void put(K key, V value) {
@@ -34,18 +32,17 @@ public class HashTable<K, V> {
             this.table[index].add(new KeyValue<>(key, value));
         }
 
-        this.table[index].add(new KeyValue<>(key, value));
         this.size++;
 
-        if (this.size >= table.length * MAX_LOAD_FACTOR) {
+        if (this.size >= this.capacity * MAX_LOAD_FACTOR) {
             this.doubleTheCapacity();
         }
     }
 
     private boolean set(LinkedList<KeyValue<K, V>> chain, K key, V value) {
         for (KeyValue<K, V> element : chain) {
-            if (element.key.equals(key)) {
-                element.value = value;
+            if (element.getKey().equals(key)) {
+                element.setValue(value);
                 return true;
             }
         }
@@ -53,8 +50,15 @@ public class HashTable<K, V> {
         return false;
     }
 
+
     private void doubleTheCapacity() {
-        //TODO
+        HashTable<K, V> newHashTable = new HashTable<>(this.capacity *= 2);
+
+        for (KeyValue<K, V> element : this) {
+            newHashTable.put(element.getKey(), element.getValue());
+        }
+
+        this.table = newHashTable.table;
     }
 
     public V get(K key) {
@@ -63,8 +67,8 @@ public class HashTable<K, V> {
 
         if (chain != null) {
             for (KeyValue<K, V> element : chain) {
-                if (element.key.equals(key)) {
-                    return element.value;
+                if (element.getKey().equals(key)) {
+                    return element.getValue();
                 }
             }
         }
@@ -80,6 +84,45 @@ public class HashTable<K, V> {
 
     @SuppressWarnings("unchecked")
     private LinkedList<KeyValue<K, V>>[] createTable(int size) {
-        return (LinkedList<KeyValue<K, V>>[]) Array.newInstance(KeyValue.class, size);
+        return (LinkedList<KeyValue<K, V>>[]) Array.newInstance(LinkedList.class, size);
+    }
+
+    private class HashIterator implements Iterator<KeyValue<K, V>> {
+        int counter = 0;
+        int pointer = 0;
+        int innerPointer = 0;
+        LinkedList<KeyValue<K, V>> chain = null;
+
+        @Override
+        public boolean hasNext() {
+            return this.counter < size;
+        }
+
+        @Override
+        public KeyValue<K, V> next() {
+            while (this.chain == null) {
+                this.chain = table[this.pointer++];
+            }
+
+            KeyValue<K, V> result = this.chain.get(this.innerPointer++);
+
+            if (this.innerPointer >= this.chain.size()) {
+                this.chain = null;
+                this.innerPointer = 0;
+            }
+
+            this.counter++;
+            return result;
+        }
+    }
+
+    @Override
+    public Iterator<KeyValue<K, V>> iterator() {
+        return new HashIterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super KeyValue<K, V>> action) {
+        Iterable.super.forEach(action);
     }
 }
